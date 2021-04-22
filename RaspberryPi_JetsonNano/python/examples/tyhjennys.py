84ef1919a30ed9e52b33d181d6066c75412a0000
Arttu Moisio <arttu.moisio@tuni.fi>
2021-04-22 23:19:55 +0300

    aren testi

---
+++ RaspberryPi_JetsonNano/python/examples/tyhjennys.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in9
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in9 Demo")
+
+    epd = epd2in9.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.lut_full_update)
+    epd.Clear(0xFF)
+
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd2in9.epdconfig.module_exit()
+    exit()
